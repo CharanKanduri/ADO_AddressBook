5b0d912b4a6478e99cec9d951c357fd5cb5d93a2
charan_kanduri <charan2000kanduri@gmail.com>
2021-08-08 15:58:01 +0530

    [CHARAN]Establish connection

---
+++ ADO_AddressBook/AddressBookRepo.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data;
+//using System.Data.SqlClient;
+//using System.Text;
+
+//namespace ADO_AddressBook
+//{
+//    class AddressBookRepo
+//    {
+//        //Give path for Database Connection
+//        public static string connection = @"Server=.;Database=AddressBookServiceDB;Trusted_Connection=True;";
+//        //Represents a connection to Sql Server Database
+//        SqlConnection sqlConnection = new SqlConnection(connection);
+//    }
+//}
---
+++ ADO_AddressBook/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ADO_AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+        }
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection("data source=.; Database=AddressBookServiceDB; Trusted_Connection=True;");
+                Console.WriteLine("Connected Successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
