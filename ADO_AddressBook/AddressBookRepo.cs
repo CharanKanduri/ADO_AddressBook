fceb38f0fddb8a20b2b3369e028b7d7b5153064e
charan_kanduri <charan2000kanduri@gmail.com>
2021-08-08 16:09:11 +0530

    [CHARAN]Establish connection

--- ADO_AddressBook/AddressBookRepo.cs
+++ ADO_AddressBook/AddressBookRepo.cs
@@ -1,16 +1,15 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Data;
-//using System.Data.SqlClient;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
 
-//namespace ADO_AddressBook
-//{
-//    class AddressBookRepo
-//    {
-//        //Give path for Database Connection
-//        public static string connection = @"Server=.;Database=AddressBookServiceDB;Trusted_Connection=True;";
-//        //Represents a connection to Sql Server Database
-//        SqlConnection sqlConnection = new SqlConnection(connection);
-//    }
-//}
+namespace ADO_AddressBook
+{
+    class AddressBookRepo
+    {   //Give path for Database Connection
+        public static string connection = @"Server=.;Database=AddressBookService_DB;Trusted_Connection=True;";
+        //Represents a connection to Sql Server Database
+        SqlConnection sqlConnection = new SqlConnection(connection);
+    }
+}
--- ADO_AddressBook/Program.cs
+++ ADO_AddressBook/Program.cs
@@ -10,26 +10,8 @@ namespace ADO_AddressBook
     {
         static void Main(string[] args)
         {
-            new Program().CreateTable();
+            Console.WriteLine("Welcome to AddressBook ADO program");
         }
-        public void CreateTable()
-        {
-            SqlConnection con = null;
-            try
-            {
-                // Creating Connection  
-                con = new SqlConnection("data source=.; Database=AddressBookServiceDB; Trusted_Connection=True;");
-                Console.WriteLine("Connected Successfully");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("OOPs, something went wrong." + e);
-            }
-            // Closing the connection  
-            finally
-            {
-                con.Close();
-            }
-        }
+       
     }
 }
